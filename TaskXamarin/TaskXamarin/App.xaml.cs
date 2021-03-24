9c04c976ab756ea59c10511779a38e3222a88489
Arlind Podrimqaku <arlind.podrimqaku@ks.asseco-see.local>
2021-03-24 16:36:40 +0100

    Add project files.

---
+++ TaskXamarin/TaskXamarin/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using TaskXamarin.View;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TaskXamarin
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new Login();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
